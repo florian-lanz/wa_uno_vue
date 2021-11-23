e948259775ad54cd35efef7fbc8844fc370e5c3d
moritzknauer <poelker.noering@gmx.de>
2021-11-23 16:43:10 +0100

    Started to work on About Page

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Comfortaa');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif, Comfortaa;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,249 @@
+<template>
+  <div class="container-md content-container">
+    <h1 class="text-center">Uno</h1>
+    <hr/>
+    <p>
+      Uno ist ein US-amerikanisches Kartenspiel, dessen primäres Ziel es ist,
+      seine Karten vergleichbar mit Mau-Mau möglichst schnell abzulegen, wobei mit einem speziellen Blatt gespielt
+      wird. Das Spiel wurde auf der Grundlage einer traditionellen Spielidee bereits 1969 von Merle Robbins –
+      einem Inhaber eines Friseursalons in Milford – mit Hilfe seiner Frau Marie, seinem Sohn
+      Ray und dessen Frau Kathy entwickelt. Anfangs wurde es nur in der eigenen Familie gespielt. Seit 1972
+      wird
+      das
+      Spiel von der Firma International Games international vermarktet.
+      <br/>
+      <br/>
+      Das Spiel besteht aus 108 Karten. Dabei gibt es Zahlen- sowie Aktionskarten in 4 verschiedenen Farben &
+      außerdem schwarze Sonderkarten. Die Zahlen- & Aktionskarten gibt es in den Farben Blau, Grün, Gelb &
+      Rot.
+      Die Zahlenkarten gehen von 0-9, wobei es die 0 in jeder Farbe nur einmal gibt und die Karten 1-9 in
+      jeder Farbe
+      2 mal. Die Aktionskarten bestehen aus einer Aussetzen-Karte, einer Richtungswechsel-Karte und einer
+      +2-Karte. Diese gibt es jeweils auch 2 mal in jeder Farbe. Die schwarzen Sonderkarten bestehen aus einer
+      Farbwechsel-Karte & einer +4-Karte, beide Sonderkarten gibt es jeweils 4 mal. Die Funktionsweise der
+      Aktions- & Sonderkarten wird unten genauer erklärt.
+      <br/>
+    </p>
+
+    <h1 class="text-center">Spielregeln</h1>
+    <hr/>
+
+    <h3 class="text-center">Ziel des Spiels</h3>
+
+    <p>
+      Das Ziel des Spiels ist es, die eigenen Handkarten vollständig abzulegen, bevor ein anderer Spieler seine
+      Karten vollständig ablegen konnte. Das Spiel kann gegen 1, 2 oder 3 Gegner gespielt werden.
+    </p>
+
+    <h3 class="text-center">Zu Beginn des Spiels</h3>
+
+    <p>
+      Bei jedem neuen Spiel erhält jeder Spieler 7 zufällige Handkarten. Die verbleibenden Karten werden verdeckt
+      in die Mitte gelegt und bilden den Kartenstapel. Vom Kartenstapel wird die oberste Karte
+      aufgedeckt und daneben gelegt. Dieser Stapel bildet den Ablegestapel. Du beginnst jede Spielrunde.
+    </p>
+
+    <h3 class="text-center">Spielverlauf</h3>
+
+    <p>
+      Der erste Spieler legt eine Karte von seiner Hand auf den Ablagestapel. Dabei gilt: Eine Karte kann nur auf
+      eine Karte der gleichen Farbe oder der gleichen Zahl gelegt werden. Kann ein Spieler keine passende Karte
+      legen, so muss er eine Strafkarte vom verdeckten Stapel ziehen. Diese Karte kann er sofort wieder
+      ausspielen, sofern diese passt. Hat er keine passende Karte ist der nächste Spieler an der Reihe. Die Runde
+      gewinnt derjenige, welcher die letzte Karte abgelegt hat.
+    </p>
+
+    <br><br>
+    <h1 class="text-center">Aktions- & Spezialkarten</h1>
+    <hr/>
+
+    <h3 class="text-center">Aktionskarten</h3>
+
+    <div class="row">
+      <div class="col-md-2 col-sm-12 my-auto">
+        <div class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../assets/R+2.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/B+2.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/G+2.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/Y+2.png" class="d-block mx-auto" width="100">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-10 col-sm-12 text-justify text-md-left">
+        <h4>Zieh Zwei Karten</h4>
+        <p>
+          Wenn diese Karte gelegt wird, muss der nächste Spieler 2 Karten ziehen und darf
+          in dieser Runde keine Karten ablegen. Diese Karte kann nur auf eine Karte mit entsprechender
+          Farbe oder andere Zieh Zwei Karten gelegt werden.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-sm-12 my-auto">
+        <div class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../assets/R_D.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/B_D.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/G_D.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/Y_D.png" class="d-block mx-auto" width="100">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-10 col-sm-12 text-justify text-md-left">
+        <h4>Richtungswechsel Karte</h4>
+        <p>
+          Bei dieser Karte ändert sich die Spielrichtung. Wenn bisher nach links gespielt wurde, wird nun nach
+          rechts gespielt und umgekehrt. Die Karte kann nur auf eine entsprechende Farbe oder eine andere
+          Retour Karte gelegt werden.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-sm-12 my-auto">
+        <div class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../assets/R_S.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/B_S.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/G_S.png" class="d-block mx-auto" width="100">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/Y_S.png" class="d-block mx-auto" width="100">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-10 col-sm-12 text-justify text-md-left">
+        <h4>Aussetzen Karte</h4>
+        <p>
+          Nachdem diese Karte gelegt wurde, wird der nächste Spieler „übersprungen“. Die Karte kann nur
+          auf eine andere mit entsprechender Farbe oder eine andere Aussetzen Karte gelegt werden.
+        </p>
+      </div>
+    </div>
+    <h3 class="text-center">Spezialkarten</h3>
+    <div class="row">
+      <div class="col-md-2 col-sm-12 my-auto">
+        <img src="../assets/S_C.png" class="mx-auto d-block" width="100">
+      </div>
+      <div class="col-md-10 col-sm-12 text-justify text-md-left">
+        <h4>Farbauswahlkarte</h4>
+        <p>
+          Der Spieler, der diese Karte legt, entscheidet welche Farbe als nächstes gelegt werden soll. Auch
+          die schon liegende Farbe darf gewählt werden. Ein Farbenwahl Karte darf auch dann gelegt werden,
+          wenn der Spieler eine andere Karte legen könnte. Wenn eine Farbenwahl Karte zu Beginn des Spiels
+          gezogen wird, entscheidet der beginnende Spieler, welche Farbe als nächstes gelegt werden soll.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-sm-12 my-auto">
+        <img src="../assets/S+4.png" class="mx-auto d-block" width="100">
+      </div>
+      <div class="col-md-10 col-sm-12 text-justify text-md-left">
+        <h4>Zieh Vier Farbauswahlkarte</h4>
+        <p>
+          Diese Karte ist die beste. Der Spieler, der sie legt, entscheidet, welche Farbe als nächstes gelegt
+          werden soll. Zudem muss der nächste Spieler 4 Karten von dem Kartenstapel nehmen. Er darf in
+          dieser Runde keine Karte ablegen. Leider darf die Karte nur dann gelegt werden, wenn der Spieler der
+          sie hat, keine Karte in der Hand hält, die der Farbe auf dem Ablegestapel entspricht. Hat der
+          Spieler eine Karte mit der entsprechenden Nummer oder Aktionskarten, kann die Zieh Vier
+          Farbauswahlkarte dennoch gelegt werden.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutComponent"
+}
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  div {
+    background-color: #141427 !important;
+  }
+
+  h1 {
+    font-family: Comfortaa;
+    color: #FFFFFF;
+    padding: 15px 15px 15px 15px;
+  }
+
+  h3 {
+    font-family: Comfortaa;
+    color: #FFFFFF;
+    padding: 15px 15px 15px 15px;
+  }
+
+  h4 {
+    font-family: Comfortaa;
+    color: #FFFFFF;
+    padding-top: 30px;
+  }
+
+  @media (max-width: 767px) {
+    h4 {
+      text-align: center;
+    }
+
+    p {
+      padding-bottom: 60px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    p {
+      padding-bottom: 15px;
+    }
+  }
+
+  p {
+    font-family: Comfortaa;
+    font-size: 16px;
+    font-weight: normal;
+    text-align: justify;
+    color: #FFFFFF;
+    padding-top: 15px;
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  .content-container {
+    padding-top: 75px;
+  }
+
+  hr {
+    color: #FFFFFF !important;
+  }
+</style>
